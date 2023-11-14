c56c1df9d2ffefc6b1396fdf187010003c7c0061
David Vaquero <pepesan@gmail.com>
2023-11-14 10:42:01 +0100

    mejorados los dto con validaciones

--- src/main/java/com/inetum/demo/dtos/Dato.java
+++ src/main/java/com/inetum/demo/dtos/Dato.java
@@ -1,6 +1,8 @@
 package com.inetum.demo.dtos;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,5 +15,10 @@ import java.io.Serializable;
 public class Dato implements Serializable {
     private Long id;
     @NotNull
+    @NotBlank
+    @Size(min = 4, max = 20, message = "Debe tener entre 1 y 100 chars")
     private String cadena;
+    public Dato(DatoDTO datoDTO) {
+        this.cadena = datoDTO.getCadena();
+    }
 }
--- src/main/java/com/inetum/demo/dtos/DatoDTO.java
+++ src/main/java/com/inetum/demo/dtos/DatoDTO.java
@@ -1,6 +1,8 @@
 package com.inetum.demo.dtos;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,5 +12,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class DatoDTO {
     @NotNull
+    @NotBlank
+    @Size(min = 4, max = 20, message = "Debe tener entre 1 y 100 chars")
     private String cadena;
 }
