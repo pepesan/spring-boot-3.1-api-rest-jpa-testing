7638fccb01968e78bbccf612a6b3fbad2354c4a0
pepesan <pepesan@gmail.com>
2024-11-12 17:57:58 +0100

    metido ejemplo de prueba basica y cambio de properties par deshabilitar flyway en testing

---
+++ src/test/java/com/inetum/demo/ApiRestJpaApplicationTests.java
@@ -0,0 +1,11 @@
+package com.inetum.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApiRestJpaApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
