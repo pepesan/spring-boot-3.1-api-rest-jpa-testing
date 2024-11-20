1487a844c74772e7c5cd6b91da78e2fe88a9f326
David <dvaquero@bilbomatica.es>
2024-11-20 18:11:03 +0100

    ejercicio resuelto de test de api rest de Cliente contra bbdd y servicio metido ejemmplo de getbyid teniendo en cuenta el no reseteo de los id de la tabla

--- src/test/java/com/inetum/demo/ejercicios/ClienteControllerTest.java
+++ src/test/java/com/inetum/demo/ejercicios/ClienteControllerTest.java
@@ -1,24 +1,20 @@
 package com.inetum.demo.ejercicios;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.inetum.demo.dtos.Dato;
-import com.inetum.demo.dtos.DatoDTO;
-import com.inetum.demo.repositories.AlumnoRepository;
-import com.mysql.cj.xdevapi.Client;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -30,7 +26,6 @@ public class ClienteControllerTest {
     public String basePath = "/api/v1/cliente";
     @Autowired
     private ClienteRepository clienteRepository;
-
     @BeforeEach
     void setUp() {
         this.clienteRepository.deleteAll();
@@ -56,7 +51,49 @@ public class ClienteControllerTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(content().json(asJsonString(new Cliente(1L,"Pilar", "p@p.com", "Madrid"))));
+                .andExpect(jsonPath("$.name").value("Pilar"))
+                .andExpect(jsonPath("$.email").value("p@p.com"))
+                .andExpect(jsonPath("$.dir").value("Madrid"))
+                .andExpect(jsonPath("$.id").exists());
+    }
+    @Test
+    void testAddShouldReturnDato2() throws Exception {
+        System.out.println("añadiendo");
+        mockMvc.perform(
+                        // configura la petición
+                        //MockMvcRequestBuilders.
+                        post(basePath+"/")
+                                .content(asJsonString(new ClienteDTO("Pilar", "p@p.com", "Madrid")))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.name").value("Pilar"))
+                .andExpect(jsonPath("$.email").value("p@p.com"))
+                .andExpect(jsonPath("$.dir").value("Madrid"))
+                .andExpect(jsonPath("$.id").exists());
+    }
+    @Test
+    void testGetByIDShouldReturnOkResult() throws Exception {
+        Cliente clienteGuardado = this.clienteRepository.save(new Cliente(1L, "Pilar", "p@.com", "Madrid"));
+        System.out.println(clienteGuardado);
+        mockMvc.perform(
+                        MockMvcRequestBuilders
+                                .get(basePath+"/"+clienteGuardado.getId())
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+    @Test
+    void testGetByIDShouldReturnNotFoundResult() throws Exception {
+
+        mockMvc.perform(
+                        MockMvcRequestBuilders
+                                .get(basePath+"/300")
+                                .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isNotFound());
     }
     public static String asJsonString(final Object obj) {
         try {
@@ -65,4 +102,5 @@ public class ClienteControllerTest {
             throw new RuntimeException(e);
         }
     }
+
 }
